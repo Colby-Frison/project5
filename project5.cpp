076194316105e1a849497d404d90c44c64979f19
Colby-Frison <colbyfrison100@gmail.com>
2024-11-18 09:03:29 -0600

    first commit

---
+++ project5.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_map>
+#include <map>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
+
+int main () {
+
+    ifstream file("input.txt");
+    if (!file.is_open()) {
+        cerr << "Error opening file\n";
+        return 1;
+    }
+
+    unordered_map<string, int> wordFrequency;
+    string line, word;
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+        while (ss >> word) {
+            wordFrequency[word]++;
+        }
+    }
+
+    file.close();
+
+    for (const auto &entry : wordFrequency) {
+        cout << entry.first << " : " << entry.second << '\n';
+    }
+
+    //read the name of the file ./a.out < filename.txt
+
+    //get each token and store them in the unordered_map (or map) increment
+    //its frequencies. You MAY be able to do myTokens[aToken]++. Work on this.
+
+    //close the file (filename.txt)
+
+
+    //sort the myTokens in the decreasing order of VALUE which is frequencies
+
+    //print the KEYS in myTokens (which has been sorted)sep arated by a space.
+    //after you printed the KEYS Do this
+    cout << endl;
+    cout << "********" << endl;
+
+
+    //Now open the filename.text file again for reading
+    //Read token by token as you have done this before
+
+    //each time you get a token, find its position in the myTokens (sorted
+    //data structure and print the position followed by space
+
+
+    cout << endl;
+
+    return 0;
+}
